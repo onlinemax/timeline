5eb54dc956e17698f262ffda1a93bd074f6d41ba
onlinemax <91437969+onlinemax@users.noreply.github.com>
2024-03-27 16:17:44 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,65 @@
+const data = [{
+            date: new Date(1945,0,1),
+            content: {
+                container: "div content",
+                image: {
+                    container: "div image-wrapper",
+                    img_url: "https://hips.hearstapps.com/hmg-prod/images/xi-jinping-GettyImages-494301010_1600.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*",
+                    alt_txt: "A random image",
+                },
+                text: {
+                    container: "div text",
+                    content: [{
+                        container: "h2",
+                        text: "Assasinat du Duc Cardinant",
+                    }, {
+                        container: "p",
+                        text: "Ceci est du texte que j'écis pour te faire comprendre quelque chose c'est pourquoi je l'écris cependant ne vas pas penser que tu puisse me battre dans mes démarches.Car la pensée elle même ne définit pas ce que nous pensons ni même les dégats de la langue, Car c'est sur ses mots que je te prééviens prend garde à ne pas perdre",
+
+                    }]
+                }
+            }
+        }, {
+            date: new Date(1949,11,15),
+            content: {
+                container: "div content invisible",
+                image: {
+                    container: "div image-wrapper",
+                    img_url: "President-of-China-Xi-Jinping.webp",
+                    alt_txt: "A random image",
+                },
+                text: {
+                    container: "div text",
+                    content: [{
+                        container: "h2",
+                        text: "Assasinat du Duc Cardinant",
+                    }, {
+                        container: "p",
+                        text: "Ceci est du texte que j'écis pour te faire comprendre quelque chose c'est pourquoi je l'écris cependant ne vas pas penser que tu puisse me battre dans mes démarches.Car la pensée elle même ne définit pas ce que nous pensons ni même les dégats de la langue, Car c'est sur ses mots que je te prééviens prend garde à ne pas perdre.",
+
+                    }]
+                }
+            },
+        }, {
+            date: new Date(1960,2,15),
+            content: {
+                container: "div content invisible",
+                image: {
+                    container: "div image-wrapper",
+                    img_url: "President-of-China-Xi-Jinping.webp",
+                    alt_txt: "A random image",
+                },
+                text: {
+                    container: "div text",
+                    content: [{
+                        container: "h2",
+                        text: "Assasinat du Duc Cardinant",
+                    }, {
+                        container: "p",
+                        text: "Ceci est du texte que j'écis pour te faire comprendre quelque chose c'est pourquoi je l'écris cependant ne vas pas penser que tu puisse me battre dans mes démarches.Car la pensée elle même ne définit pas ce que nous pensons ni même les dégats de la langue, Car c'est sur ses mots que je te prééviens prend garde à ne pas perdre.",
+
+                    }]
+                }
+            }
+        }
+    ];
