cb595bb01767189bb678f99e889681d05f303029
onlinemax <91437969+onlinemax@users.noreply.github.com>
2024-04-01 20:30:17 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,65 +1,134 @@
 const data = [{
-            date: new Date(1945,0,1),
+            date: new Date(1949,0,1),
+            dateString: "Le 1er Janvier 1949",
             content: {
                 container: "div content",
                 image: {
                     container: "div image-wrapper",
-                    img_url: "https://hips.hearstapps.com/hmg-prod/images/xi-jinping-GettyImages-494301010_1600.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*",
-                    alt_txt: "A random image",
+                    img_url: "https://thechinaproject.com/wp-content/uploads/2021/08/Chiang-Kai-shek-and-Mao-Zedong.jpg",
+                    alt_txt: "Mao Zedong révolutionnaire et le général Chiang Kai Chek lors de leur dernière rencontre",
+                    label: "Mao Zedong révolutionnaire et le général Chiang Kai Chek lors de leur dernière rencontre"
                 },
-                text: {
+            text: {
                     container: "div text",
                     content: [{
                         container: "h2",
-                        text: "Assasinat du Duc Cardinant",
+                        text: "1949 - Défausse de la Chine par le Japon ",
                     }, {
                         container: "p",
-                        text: "Ceci est du texte que j'écis pour te faire comprendre quelque chose c'est pourquoi je l'écris cependant ne vas pas penser que tu puisse me battre dans mes démarches.Car la pensée elle même ne définit pas ce que nous pensons ni même les dégats de la langue, Car c'est sur ses mots que je te prééviens prend garde à ne pas perdre",
-
+                        text: "Lors de la fin de la deuxième Guerre Mondiale, les Japonais, qui occupaient la Chine, ainsi que Taiwan ont été obligés de redonner l’île à la Chine. Cependant, après la guerre, à cause du massacre des japonais, ayant tué plus de 7.5 millions de chinois lors de la guerre, les communistes de Mao Zedong, prônant le partage des terres avec le peuple, se sont rebellés contre le gouvernement nationaliste chinois et ceux-ci ont été soutenus par l’URRS. Ils ont renversé le gouvernement en place et vaincu les nationalistes du général Chiang Kai-Shek, qui, le 8 décembre 1949, a décidé de se réfugier à Taiwan avec ce qu’il restait de son gouvernement, à plus de 150 km de la côte chinoise. Depuis ce jour, le gouvernement de Pékin tenterait de réunifier Taiwan et la Chine.",
                     }]
                 }
             }
         }, {
-            date: new Date(1949,11,15),
+            date: new Date(1955, 5,1),
             content: {
                 container: "div content invisible",
                 image: {
                     container: "div image-wrapper",
-                    img_url: "President-of-China-Xi-Jinping.webp",
-                    alt_txt: "A random image",
+                    img_url: "https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/c99f/live/4a3b44f0-195b-11ed-894d-e96102bbb308.jpg",
+                    label: "Le leader nationaliste Chiang Kai-shek passe en revue les troupes américaines à bord du USS Wasp dans les eaux territoriales de Taïwan.",
                 },
                 text: {
                     container: "div text",
                     content: [{
                         container: "h2",
-                        text: "Assasinat du Duc Cardinant",
+                        text: "1955 - Première crise du détroit de Taiwan",
                     }, {
                         container: "p",
-                        text: "Ceci est du texte que j'écis pour te faire comprendre quelque chose c'est pourquoi je l'écris cependant ne vas pas penser que tu puisse me battre dans mes démarches.Car la pensée elle même ne définit pas ce que nous pensons ni même les dégats de la langue, Car c'est sur ses mots que je te prééviens prend garde à ne pas perdre.",
+                        text: "Les partisans de Mao Zedong vont prendre possession de quelques îles sur le territoire de Taiwan. Cependant, ils seront vite freinée par les États-Unis, qui considère le gouvernement nationaliste présent en Taiwan comme le seul gouvernement  légitime chinois, vont menacer pékin, notamment avec la résolution de la Fermose du 29 janvier 1955, permettant au président du pays à rétablir par la force si nécéssaire la paix dans le détroit. Devant ce puissant allié et une possible menace de force nucléaire par ceux-ci, les envahisseurs renoncent à leur tentative d’annexion de Taiwan.",
 
                     }]
                 }
             },
         }, {
-            date: new Date(1960,2,15),
+            date: new Date(1958,2,15),
             content: {
                 container: "div content invisible",
                 image: {
                     container: "div image-wrapper",
-                    img_url: "President-of-China-Xi-Jinping.webp",
+                    img_url: "https://amcham.com.tw/wp-content/uploads/2019/03/tra-2.jpg",
+                    alt_txt: "A random image",
+                    label:"This is an image",
+                },
+                text: {
+                    container: "div text",
+                    content: [{
+                        container: "h2",
+                        text: "1958- Deuxième crise du détroit de Taiwan",
+                    }, {
+                        container: "p",
+                        text: "Afin de vérifier la détermination des Américains à protéger Taiwan, Mao Zedong a décidé de bombarder les îles de Quemoy et de Matsu. Ceux-ci ont réagi rapidement en mettant en place une flotte militaire à Taiwan et en ravitaillant Taiwan avec de l'artillerie militaire, pouvant tirer des charges nucléaires.",
+
+                    }]
+                }
+            }
+        },
+        {
+            date: new Date(1979,1,15),
+            content: {
+                container: "div content invisible",
+                image: {
+                    container: "div image-wrapper",
+                    img_url: "https://amcham.com.tw/wp-content/uploads/2019/03/tra-2.jpg",
                     alt_txt: "A random image",
                 },
                 text: {
                     container: "div text",
                     content: [{
                         container: "h2",
-                        text: "Assasinat du Duc Cardinant",
+                        text: "1979- Vote du «Taiwan Relation Act",
                     }, {
                         container: "p",
-                        text: "Ceci est du texte que j'écis pour te faire comprendre quelque chose c'est pourquoi je l'écris cependant ne vas pas penser que tu puisse me battre dans mes démarches.Car la pensée elle même ne définit pas ce que nous pensons ni même les dégats de la langue, Car c'est sur ses mots que je te prééviens prend garde à ne pas perdre.",
-
+                        text: "Le congrès américain garantit à Taiwan de l’aide pour la protection de leur territoire, ainsi qu’une entraide économique pour faciliter le commerce entre les deux nations.",
+                    }]
+                }
+            }
+            },
+            {
+            date: new Date(1987,6,15),
+            content: {
+                container: "div content invisible",
+                image: {
+                    container: "div image-wrapper",
+                    img_url: "https://upload.wikimedia.org/wikipedia/en/thumb/6/63/Tsmc.svg/1200px-Tsmc.svg.png",
+                    alt_txt: "A random image",
+                    label:"",
+                },
+                text: {
+                    container: "div text",
+                    content: [{
+                        container: "h2",
+                        text: "1987: Création de la compagnie de puces électronique Taiwan Semiconductor Manufacturing Company(TSMC) par le gouvernement taiwanais.",
+                    }, {
+                        container: "p",
+                        text: "Cette date est importante, car elle marque le début d’une compagnie qui est un acteur très important dans les tensions entre Taiwan et la Chine. Dans le processus de la création d’électronique, il est vital d’avoir des puces de très grandes qualités pour assurer un bon fonctionnement de l’appareil. Cependant, la création de puces coûte chère et n’est pas viable pour la plupart des «start-ups companies» en quête de nouvelles technologies. C’est pourquoi TSMC a été fondé. Cette compagnie s’occupe de la création de puces et tout ce que les clients doivent faire est de simplement lui fournir le plan de conception de la puce désirée. Ainsi, TSMC est devenue une des meilleures compagnies de création de puces dans le monde. On estime que plus de 60% de nos puces proviennent de cette compagnie et qu’elle produit 92% des puces les plus performantes sur le marché. La Chine, à cause des restrictions imposées par les États-Unis, cherche une nouvelle raison pour prendre possession de Taiwan et, par conséquent, se procurer des puces performantes. Certains d’entre vous allez vous poser la question suivante: pourquoi la Chine ne produit-elle pas ses propres puces à la place? La réponse s’explique par la loi de Moores (Moore’s law). Cette loi très simple de Gordon Earle Moore, le cofondateur d’Intel, une compagnie de puces informatiques, précise qu'à tous les deux ans, le nombre de transistors dans une puce électronique double. Un transistor est un des principaux composants de tout appareil électronique. Simplement dit, plus un appareil possède de transistors, plus la performance de l’appareil sera significative. Étant donné que la Chine est entrée dans la course à la création des meilleures puces quelques dizaines d’années après les États-Unis, les puces créées par l’Occident possèdent 32 fois plus de transistors que la Chine. Sachant qu’il peut y avoir des milliards de transistors dans une seule puce, c’est un très grand retard qu’a la Chine, expliquant sa motivation à rapatrier Taiwan, l’endroit où les puces supérieures sont fabriquées."
+                    }]
+                }
+            }
+        },
+        {
+            date: new Date(1996,5,15),
+            content: {
+                container: "div content invisible",
+                image: {
+                    container: "div image-wrapper",
+                    img_url: "https://cdn-hub.ina.fr/notice/360x270/0ec/AFE85005980.jpeg?__=95386_95380",
+                    alt_txt: "A random image",
+                    label:"",
+                },
+                text: {
+                    container: "div text",
+                    content: [{
+                        container: "h2",
+                        text: "1996- Troisième crise de Taiwan",
+                    }, {
+                        container: "p",
+                        text: "Après la mort de Chiang Ching-Kuo, fils de Chiang-Kai-Shek, Taiwan est devenu une démocratie et Lee Teng-Hui est devenu président de la République de Chine. De plus, avec la visite aux États-Unis du nouveau président taiwannais, le gouvernement chinois, voyant qu’il a perdu de l’influence sur ce pays, a fait des tirs à blanc près de la côte taiwannaise afin de l’intimider. Cependant, grâce à la réaction rapide du président Clinton, aucun affrontement ne s’est produit. De fait, l’ancien président américain a envoyé des porte-avions au pays allié pour décourager la Chine à attaquer Taiwan, ce qui a porté fruit."
                     }]
                 }
             }
         }
+
+
     ];
